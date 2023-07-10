b6e381d928e01ca73831f3d267ab41c8734fdd42
HP <HP@DESKTOP-ASRE6C9>
2023-07-10 09:50:57 +0530

    first commit

---
+++ C13/src/com/cg/dayone/emo.java
@@ -0,0 +1,19 @@
+package com.cg.dayone;
+
+public class emo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Printing emoji: \uD83D\uDE03");
+		 System.out.println("Printing emoji: \uD83D\uDE0A");
+		 System.out.println("Printing emoji: \uD83D\uDE02");
+		 System.out.println("Printing emoji: \uD83D\uDE09");
+		 System.out.println("Printing emoji: \uD83D\uDE18");
+		 System.out.println("Printing emoji: \uD83D\uDE3B");
+		 System.out.println("Printing emoji: \uD83D\uDE21");
+		 System.out.println("Printing emoji: \uD83D\uDE82");
+
+
+	}
+
+}
