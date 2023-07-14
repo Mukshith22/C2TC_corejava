2ef5d01175a1c967755f496910ea4ee326ebb12f
HP <HP@DESKTOP-ASRE6C9>
2023-07-14 14:34:03 +0530

    third submit

---
+++ C13/src/com/cg/dayfive/Single/SingleDemo.java
@@ -0,0 +1,20 @@
+package com.cg.dayfive.Single;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+        Student s = new Student();
+		
+		s.setName("Mukshith");
+		s.setCollege("MSAJCE");
+		s.setAadhar("123045693210");
+		s.setArea("Mayiladuthurai");
+		s.setPhNo(999417403);
+		s.setRollNo(6014);
+		
+		System.out.println(s);
+
+
+	}
+
+}
